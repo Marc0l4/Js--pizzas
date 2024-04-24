543b7c1d90cc1bf6519e28609465927b2680e6cd
Marco Francisco de Oliveira Junior <167469636+Marc0l4@users.noreply.github.com>
2024-04-24 14:00:16 -0300

    Add files via upload

---
+++ pizzas.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'images/pizza.png', price:42.00, sizes:['4 p/', '8 p/', '16 p/'], description:'Uma pizza bem recheada de mussarela e tomate'},
+    {id:2, name:'Calabresa', img:'images/pizza2.png', price:36.55, sizes:['4 p/', '8 p/', '16 p/'], description:'Muita calabresa com mussarela e cebolas'},
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:43.99, sizes:['4 p/', '8 p/', '16 p/'], description:'Uma explosão de sabores com Mussarela, catupiry, gorgonzola e provolone'},
+    {id:4, name:'Americana', img:'images/pizza4.png', price:42.68, sizes:['4 p/', '8 p/', '16 p/'], description:'A classica pizza Americana conta com mussarela, presunto, ervilha, palmito e bacon'},
+    {id:5, name:'Suiça', img:'images/pizza5.png', price:40.27, sizes:['4 p/', '8 p/', '16 p/'], description:'A incrivel pizza Suiça conta com mussarela, peito de peru, bacon, palmito e milho'},
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:46.43, sizes:['4 p/', '8 p/', '16 p/'], description:'Não podia faltar a pizza da casa que conta com Mussarela, peito de peru, milho, cebola, catupiry e bacon'},
+    {id:7, name:'Confeti', img:'images/pizza7.png', price:32.22, sizes:['4 p/', '8 p/', '16 p/'], description:'Uma otima opção de pizza doce de muito chocolate e confeti'}
+];
